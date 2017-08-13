a16f0bb4defa1dc1af948fc7058b41ba81d842ed
Kuart1ss <wangxin0938@vip.qq.com>
2017-08-14 01:51:39 +0800

    1st program in python

---
+++ Ca.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import  sys
+
+while 1:
+ UserIntention = input('If you want to continue,press y and enter.'
+                       '\nFor help,press h and enter.'
+                       '\nIf not,press any other key and enter.')
+ if UserIntention == 'y':
+      FirNum = float(input('The First Number:'))
+      SecNum = float(input('The Second Number:'))
+      WhatDoYouWantToDo = input('You want to...(Add/Min/Mult/Div/Inv):')
+
+      if WhatDoYouWantToDo == 'Add':
+         NowAnswer = FirNum + SecNum
+         print(FirNum, '+', SecNum, '=', NowAnswer)
+      elif WhatDoYouWantToDo == 'Min':
+         NowAnswer = FirNum - SecNum
+         print(FirNum, '-', SecNum, '=', NowAnswer)
+      elif WhatDoYouWantToDo == 'Mult':
+         NowAnswer = FirNum * SecNum
+         print(FirNum, 'x', SecNum, '=', NowAnswer)
+      elif WhatDoYouWantToDo == 'Div':
+         NowAnswer = FirNum / SecNum
+         print(FirNum, '/', SecNum, '=', NowAnswer)
+      elif WhatDoYouWantToDo == 'Inv':
+         NowAnswer = FirNum ** SecNum
+         print(FirNum, '^', SecNum, '=', NowAnswer)
+      else:
+         print('ERROR!\nCOMMAND NOT FIND!\nPlease Enter true command.')
+ elif UserIntention == 'h':
+     print('When the program shows "The First Number",enter the nums what is in front of the equation.'
+           '\nWhen the program shows "The Second Number",enter the nums what is behind of the equation.\n'
+           'And now enter the operation type(Add/Min/Mult/Div/Inv),enter,you can get it on your screen.')
+ else:
+   print('The program is shutdown.')
+   break
